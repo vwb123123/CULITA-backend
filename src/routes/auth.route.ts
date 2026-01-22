45b1c54e0c01444b4363bdc6758cd684f22fc0a6
TaeGaram (Office) <drasla217@gmail.com>
2026-01-22 17:39:30 +0900

    Remove JWT middleware from authentication routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller';
-import { authenticateJwt } from "../middlewares/auth.middleware";
 
 const authRoute = Router();
 const authController = new AuthController();
 
-authRoute.use(authenticateJwt);
 authRoute.post('/register', authController.register);
 authRoute.post('/login', authController.login);
 
